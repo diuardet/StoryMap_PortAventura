198f251850883ea4a41046950be170026e8409d5
diuardet <diuardet@gmail.com>
2017-04-02 17:56:49 +0200

    Initial commit

---
+++ MapTour/src/lib/PortAventura.js
@@ -0,0 +1,26 @@
+PIN_CFG: {
+  k: {
+    iconPath: 'resources/markers/black/NumberIconK',
+    cssClass: 'number-black'
+  },
+  m: {
+    iconPath: 'resources/markers/magenta/NumberIconM',
+    cssClass: 'number-magenta'
+  },
+  o: {
+    iconPath: 'resources/markers/orange/NumberIconO',
+    cssClass: 'number-orange'
+  },
+  s: {
+    iconPath: 'resources/markers/salmon/NumberIconS',
+    cssClass: 'number-salmon'
+  },
+  t: {
+    iconPath: 'resources/markers/tangerine/NumberIconT',
+    cssClass: 'number-tangerine'
+  },
+  y: {
+    iconPath: 'resources/markers/tangerine/NumberIconY',
+    cssClass: 'number-yellow'
+  }
+},
---
+++ MapTour/src/resources/markers_backUp/_readme.txt
@@ -0,0 +1,25 @@
+ 
+Color        Number range  Web color  iconPrefix
+-------      ------------  ---------  -----------------
+
+Default colors
+
+green        1 to 300      #22880d    NumberIcong
+red          1 to 300      #fd2d29    NumberIconr
+blue         1 to 200      #177ff1    NumberIconb
+purple       1 to 200      #9c46fd    NumberIconp
+black        1 to 200      #000000    NumberIconK
+
+Extended colors
+
+magenta      1 to 200      #b454cb    NumberIconM
+orange       1 to 200      #e88516    NumberIconO
+salmon       1 to 200      #C96A04    NumberIconS
+tangerine    1 to 100      #FB921A    NumberIconT
+
+
+
+
+
+
+
